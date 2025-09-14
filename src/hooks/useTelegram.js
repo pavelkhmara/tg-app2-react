d8ebeeef25ecaa87c14524654840920610da8a90
Pavel Khmara <375295560023pk@gmail.com>
2025-09-14 19:06:12 +0200

    tg hook

---
+++ src/hooks/useTelegram.js
@@ -0,0 +1,23 @@
+
+export function useTelegram() {
+    const tg = window.Telegram.WebApp;
+
+    const onClose = () => {
+        tg.close();
+    }
+
+    const onToggleButton = () => {
+        if (tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }
+
+    return {
+        tg,
+        onClose,
+        onToggleButton,
+        user: tg.initDataUnsafe?.user
+    };
+}
