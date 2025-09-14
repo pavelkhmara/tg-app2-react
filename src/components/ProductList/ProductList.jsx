b9242be4510a8cac1c098c0a3341c9c116576f57
Pavel Khmara <375295560023pk@gmail.com>
2025-09-14 19:53:50 +0200

    fill productList

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,12 +1,71 @@
 import React from 'react';
 import './ProductList.css';
+import ProductItem from '../ProductItem/ProductItem';
+import { useTelegram } from '../../hooks/useTelegram';
 
-const ProductList = () => {
-  return (
-    <div>
-      ProductList
-    </div>
-  );
+const products = [
+  {id: '1', title: 'Juice', price: 200, description: 'Fresh juice 1l'},
+  {id: '2', title: 'Cola', price: 150, description: 'Coca-Cola 0.5l'},
+  {id: '3', title: 'Water', price: 100, description: 'Pure water 0.5l'},
+  {id: '4', title: 'Fanta', price: 170, description: 'Fanta 0.5l'},
+    {id: '5', title: 'Jaket', price: 1200, description: 'Winter jaket'},
+    {id: '6', title: 'Socks', price: 300, description: 'Warm socks'},
+    {id: '7', title: 'Hat', price: 250, description: 'Winter hat'},
+    {id: '8', title: 'Boots', price: 3500, description: 'Winter boots'},
+    {id: '9', title: 'Gloves', price: 400, description: 'Warm gloves'},
+    {id: '10', title: 'Scarf', price: 350, description: 'Wool scarf'},
+];
+
+const getTotalPrice = (items) => {
+    return items.reduce((acc, item) => {
+        return acc += item.price;
+    }, 0);
+}
+
+const ProductList = ({product, onRemove}) => {
+    const [ addedItems, setAddedItems ] = React.useState([]);
+    const { tg } = useTelegram();
+
+    const onAdd = (product) => {
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
+
+        if(alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id);
+        } else {
+            newItems = [...addedItems, product];
+        }
+
+        setAddedItems(newItems);
+
+        if(newItems.length === 0) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.setParams({
+                text: `Buy ${getTotalPrice(newItems)} items`
+            });
+            tg.MainButton.show();
+        }
+    }
+
+    React.useEffect(() => {
+        setAddedItems(product);
+    }, [product]);
+
+    return (
+        <div className={'list'}>
+            {products.map(item => (
+                <ProductItem
+                    product={item}
+                    onAdd={onAdd}
+                    onRemove={onRemove}
+                    className={'item'}
+                    key={item.id}
+                />
+            ))}
+          
+        </div>
+    );
 };
 
 export default ProductList;
